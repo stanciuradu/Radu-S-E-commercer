2ea0c26a929b257f39d62a1b1f38a970fb301322
radu <radustanciu841@yahoo.ro>
2021-05-30 13:15:49 +0300

    modify firebase

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,11 @@
 // acesta este un obiect de configurare by default care va fi inlocuit cu datele proprii din consola Firebase
 // acest obiect are datele reale, autenticie din API-ul Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBuXRVOImtlsC3EMi2fkyO11xk8alSpo6g",
-  authDomain: "autentificare-bf2fc.firebaseapp.com",
-  projectId: "autentificare-bf2fc",
-  storageBucket: "autentificare-bf2fc.appspot.com",
-  messagingSenderId: "81964085628",
-  appId: "1:81964085628:web:a66b3b19106089ecddf4f9",
+  apiKey: "TODO",
+  authDomain: "TODO",
+  projectId: "TODO",
+  storageBucket: "TODO",
+  messagingSenderId: "TODO",
+  appId: "TODO",
 };
 export default firebaseConfig;
